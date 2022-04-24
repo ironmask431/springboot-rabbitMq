132cb27bb8619b9e97dd2e11898e27e56eb9a9f9
ironmask431 <ironmask431@gmail.com>
2022-04-24 20:37:19 +0900

    rabbitMQ sender 테스트

---
+++ src/main/java/com/leesh/rabbitMq/module/SenderModule.java
@@ -0,0 +1,27 @@
+package com.leesh.rabbitMq.module;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class SenderModule {
+
+    final RabbitTemplate rabbitTemplate;
+
+    @Scheduled(fixedRate = 1000) //100ms마다 실행
+    public void sender1(){
+        // System.out.println("주기적으로 실행");
+
+        LocalDateTime nowDateTime = LocalDateTime.now();
+        String time = nowDateTime.toString();
+
+        System.out.println("mq로 time 전송 : "+time);
+        //mq로 메세지 전송.
+        rabbitTemplate.convertAndSend("time","time-first",time);
+    }
+}
